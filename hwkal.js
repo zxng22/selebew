8dc6aca504b44e8f387dc9fe4905e5e2dcf6d9e9
YT-DANIXD <117894945+YT-DANIXD@users.noreply.github.com>
2022-11-26 17:23:55 +0800

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -26,16 +26,16 @@ global.ig = '@hikal857' // ubah aja
 global.email = 'denyp857@gmail.com'
 global.region = 'indonesia'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'banghw☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'banghw☆' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'banghw☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Dani Maker☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Dani Maker☆' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Dani Maker☆' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312'] // ubah aja pake nomor lu
-global.premium = ['6281214281312'] // ubah aja pake nomor lu
-global.packname = 'banghw☆' // ubah aja
-global.ownerr = ['banghw☆'] // ubaha aja
-global.author = 'banghw☆' //ubah aja
+global.owner = ['6281251549899','6281348106274'] // ubah aja pake nomor lu
+global.premium = ['6281251549899','6281348106274'] // ubah aja pake nomor lu
+global.packname = 'Dani Maker☆' // ubah aja
+global.ownerr = ['Dani Maker☆'] // ubaha aja
+global.author = 'Dani Maker☆' //ubah aja
 global.sessionName = './sessionye/haikal' //Gausaj Di Ubah
 global.prefa = ['','!','.',',','🐤','🗿'] // Gausah Mendingan 
 global.sp = '⭔' // Gausah Juga
